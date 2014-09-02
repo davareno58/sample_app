7230cad3c877609ff023d1254366c5f395f95e42
davareno58 <davareno58@gmail.com>
2014-09-02 01:08:35 -0700

    Finish static pages

--- config/application.rb
+++ config/application.rb
@@ -26,5 +26,8 @@ module SampleApp
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+#Remove non-working color codes from rspec output:
+    #config.colorize_logging = false
   end
 end
+
--- config/environments/test.rb
+++ config/environments/test.rb
@@ -1,4 +1,4 @@
-Rails.application.configure do
+SampleApp::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # The test environment is used exclusively to run your application's
@@ -36,4 +36,7 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+  # Removes non-working color codes from rspec output:
+  #config.colorize_logging = false 
 end
+
